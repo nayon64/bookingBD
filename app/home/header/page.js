6af3f5bd9f180f52c205c13b9c4aef1bdd2e0499
Nayon <nayonroy64@gmail.com>
2024-05-14 22:56:41 +0600

    nav bar create

--- app/home/header/page.js
+++ app/home/header/page.js
@@ -1,5 +1,11 @@
+import Image from "next/image";
+import worldMap  from  "../../assets/picture/worldMap.jpg"
+
 export default function Header() {
 	return <>
+		<section>
+			<Image src={worldMap} alt="World  Map" className="max-h-96 object-cover"/>
+		</section>
 		<h1>Banner section</h1>
 	</>
 }
--- app/shared/nav/page.js
+++ app/shared/nav/page.js
@@ -1,17 +1,37 @@
 import Link from "next/link";
+import { CgShoppingBag } from "react-icons/cg";
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex  gap-5 pb-3 mb-3 border-b-2 border-black">
+    <nav className="bg-white px-3 py-3 items-center text-sm flex justify-between">
+      <div className="font-bold">
+        <span className="text-2xl">Booking </span>
+        <span className="text-green-700">Free</span>
+      </div>
+      <ul className="flex  gap-5   text-gray-800">
         <li>
           <Link href={"/"}>Home</Link>
         </li>
         <li>
           <Link href={"/about"}>About</Link>
         </li>
-        <li></li>
+        <li>
+          <Link href={"/"}>Destination</Link>
+        </li>
+        <li>
+          <Link href={"/about"}>Pages</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/about"}>Contact</Link>
+        </li>
       </ul>
+      <div className="flex gap-3 items-center">
+        <CgShoppingBag className="text-xl text-green-700 "/>
+        <Link href="/about" className="bg-green-700 text-white  py-2 px-3 rounded">Contact Us</Link>
+      </div>
     </nav>
   );
 }
