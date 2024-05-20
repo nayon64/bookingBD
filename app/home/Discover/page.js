27c0a7073248a618967eaf245fc9397bfe80e19f
Nayon <nayonroy64@gmail.com>
2024-05-20 22:57:56 +0600

    add some details

--- app/home/Discover/page.js
+++ app/home/Discover/page.js
@@ -10,6 +10,7 @@ export default function Discover() {
           "https://images.pexels.com/photos/20107285/pexels-photo-20107285/free-photo-of-boat-on-bled-lake-in-slovenia.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
         }
       />
+      <p>hello world  </p>
     </section>
   );
 }
